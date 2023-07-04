29e801d1c26f6c6829f0e868617089868b2cb73e
chandramouli02 <chandramoulirotta11@gmail.com>
2023-07-04 11:04:57 +0000

    react.js 2

---
+++ index.js
@@ -0,0 +1,13 @@
+const element = (
+  <div class="main-container">
+    <h1>Congratulations</h1>
+    <div class="card-container">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h1 class="heading">Kiran V</h1>
+      <p>Aditya Institute of technology Surampalem</p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
