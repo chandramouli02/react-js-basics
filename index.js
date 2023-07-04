382d71bc9cd5372ea845cbf52acc07598d726067
chandramouli02 <chandramoulirotta11@gmail.com>
2023-07-04 10:48:25 +0000

    react.js 1

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div>
+    <h1>Super Over League</h1>
+    <div class="images">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        alt="rcbImg"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        alt="cskImg"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
